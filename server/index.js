36e9a3ca346c1addfa6b4682c6dcd6fa83e23dd9
Vinit Sikri <vinitsikri155@gmail.com>
2024-07-12 01:55:07 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app=express()
 app.use(cors())
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
-app.use('/uploads',express.static(path.join('uploads')))
+app.use('/uploads',express.static(path.join('/uploads')))
 
 
 //video call addition
